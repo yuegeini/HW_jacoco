4337ed4834a6ce46013a05a06026c7fd6fa4146f
Name <barmatine99@gmail.com>
2023-08-26 21:55:17 +0300

    Add jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,15 @@ class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @org.junit.jupiter.api.Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
